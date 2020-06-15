01fc3fde2c86609f390f92b3c307d4e5a03976d8
Juan Eduardo Domingos <juan.eduardo@outlook.com.br>
2020-06-15 18:32:54 -0300

    Feature: Added missing architeture files and some adjustments on compose yaml

--- twitter-scraper/src/database/cassandra.connector.js
+++ twitter-scraper/src/database/cassandra.connector.js
@@ -1,1 +1,7 @@
-const cassandra = require('cassandra-driver'); 
+const cassandra = require('cassandra-driver'); \
+
+var authProvider = new cassandra.auth.PlainTextAuthProvider('Username', 'Password');
+//Replace PublicIP with the IP addresses of your clusters
+var contactPoints = ['PublicIP','PublicIP','PublicIP’'];
+var client = new cassandra.Client({contactPoints: contactPoints, authProvider: authProvider, keyspace:'grocery'});
+ 
